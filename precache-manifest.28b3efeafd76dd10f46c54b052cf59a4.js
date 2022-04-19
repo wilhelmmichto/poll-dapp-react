4379410f0d8b76633781efda0c8b73ca34e6e8ec
wilhelmmichto <michto15897@gmail.com>
2022-04-19 05:43:48 +0530

    Updates

---
+++ precache-manifest.28b3efeafd76dd10f46c54b052cf59a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8871bd94b8ab5678066",
+    "url": "/defi-staking-app/static/css/main.de75d93f.chunk.css"
+  },
+  {
+    "revision": "d8871bd94b8ab5678066",
+    "url": "/defi-staking-app/static/js/main.d8871bd9.chunk.js"
+  },
+  {
+    "revision": "08be1b3586e0b37c70cc",
+    "url": "/defi-staking-app/static/css/1.0552f8f3.chunk.css"
+  },
+  {
+    "revision": "08be1b3586e0b37c70cc",
+    "url": "/defi-staking-app/static/js/1.08be1b35.chunk.js"
+  },
+  {
+    "revision": "7145792dd01e6798ba67",
+    "url": "/defi-staking-app/static/js/runtime~main.7145792d.js"
+  },
+  {
+    "revision": "f1c9c51d6371e721cf85dfd32065de4f",
+    "url": "/defi-staking-app/index.html"
+  }
+];
